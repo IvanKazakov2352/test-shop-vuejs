054c830da9c260a8889eb2137a74b90c5a826513
IvanKazakov2352 <testkazkov@mail.ru>
2021-10-27 11:42:35 +0300

    add card product and style

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -1,8 +1,35 @@
 <template>
-  <div class="product">
-    <div class="product-photo"></div>
-    <div class="product-name"></div>
-    <div class="product-description"></div>
-    <div class="product-price"></div>
+  <div
+    class="product"
+    ref="product"
+    @mouseover="flexButtonDelete"
+    @mouseleave="noneButtonDelete"
+  >
+    <button class="product-button-delete" ref="button">
+      <img class="product-delete-icon" src="../assets/delete-icon-white.png" />
+    </button>
+    <img
+      class="product-photo"
+      src="https://richcoward.com/wp-content/uploads/2021/08/product-development.png"
+    />
+    <h1 class="product-name">Наименование товара</h1>
+    <p class="product-description">
+      Довольно-таки интересное описание товара в несколько строк. Довольно-таки
+      интересное описание товара в несколько строк
+    </p>
+    <h1 class="product-price">12 000 руб</h1>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    flexButtonDelete() {
+      this.$refs.button.style.display = "flex";
+      this.$refs.product.style.cursor = "pointer";
+    },
+    noneButtonDelete() {
+      this.$refs.button.style.display = "none";
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueMask from 'v-mask'
 import "./scss/index.scss"
+Vue.use(VueMask);
 
 Vue.config.productionTip = false
 
